93cb6ad36e5c7087a8b1face1783eee7f8f21328
Roman Bielokon <roman_slavyansk@mail.ru>
2013-11-24 19:19:19 +0200

    created project

---
+++ src/com/kvest/twittermonitor/ui/activity/TweetsListActivity.java
@@ -0,0 +1,16 @@
+package com.kvest.twittermonitor.ui.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.kvest.twittermonitor.R;
+
+public class TweetsListActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
